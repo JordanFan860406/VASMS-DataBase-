8418ceb3ee0c7dd45306a0922a711aa3bb0c9422
tommy1681 <tommy1681@gmail.com>
2017-06-14 22:19:54 +0800

    Member_DB更新

--- src/Object/member.java
+++ src/Object/member.java
@@ -4,11 +4,39 @@ public class member {
 	String name;
 	String ID;
 	time birthday;
+	int pay=0;
 	
 	
+	
+	public member(){
+		
+	}
 	public member(String name,String ID,time birthday){
 		this.name=name;
 		this.ID=ID;
 		this.birthday=birthday;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setID(String ID){
+		this.ID=ID;
+	}
+	public void setID(time birthday){
+		this.birthday=birthday;
+	}
+	public void setPay(int pay){
+		this.pay=pay;
+	}
+	
+	public String  getName(){
+		return name;
+	}
+	public String  getID(){
+		return ID;
+	}
+	public time  getBir(){
+		return birthday;
 	}
 }
